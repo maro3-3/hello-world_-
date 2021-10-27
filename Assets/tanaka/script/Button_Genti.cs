f31ba6856f3b27c32171bd1a30fba86e014ac8dc
maro <maronn.jan@gmail.com>
2021-10-27 23:35:03 +0900

    現地へ行く追加

---
+++ Assets/tanaka/script/Button_Genti.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class Button_Genti : MonoBehaviour
+{
+    [SerializeField] GameObject genti;
+  //  [SerializeField] int scenenum;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        //ボタンを表示させる----------
+        genti.SetActive(true);
+        //--------------------------
+        //非表示にする----------------------------
+       // this.gameObject.SetActive(false);
+        //---------------------------------------
+      //  SceneManager.LoadScene(scenenum);
+    }
+}
