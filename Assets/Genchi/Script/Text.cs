7d3b43d4dd86d8f72465e32f1f56d9abd3c42b9f
DESKTOP-2SEMSH8\taiby <taiby42753@i.softbank.jp>
2021-10-28 10:50:42 +0900

    no message

---
+++ Assets/Genchi/Script/Text.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour
+{
+    // Start is called before the first frame update
+   // public Text text;
+   // public string teststring;
+    //public Button button;
+    [SerializeField] GameObject Object;
+
+    [SerializeField] GameObject PLAYER;
+    void Start()
+    {
+        PLAYER = GameObject.FindGameObjectWithTag("player");
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == PLAYER)
+        {
+            Object.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Object.SetActive(false);
+    }
+
+}
