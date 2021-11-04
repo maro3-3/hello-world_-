cda06c416cf62d2bc12ef3cb8a999190def720d7
Asha0808 <amrgjm@gmail.com>
2021-11-04 14:58:53 +0900

    turn

---
+++ Assets/ishikado/DoroneAction_ishikado.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoroneAction_ishikado : MonoBehaviour
+{
+    public void WindowActive()
+    {
+        gameObject.SetActive(true);//表示
+    }
+
+    public void WindowHide()
+    {
+        gameObject.SetActive(false);//非表示
+    }
+}
---
+++ Assets/ishikado/TurnManager_ishikado.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager_ishikado : MonoBehaviour
+{
+    private int TurnNum;
+    Turn turn = new Turn();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ゲッター
+        TurnNum = turn.GetSetTurnNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TurnUp()
+    {
+        //ターン終了時
+
+        TurnNum++;
+
+        //セッター
+        turn.GetSetTurnNum = TurnNum;
+
+        //ターン開始時
+    }
+}
