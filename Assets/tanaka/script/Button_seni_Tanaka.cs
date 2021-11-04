84c944e57715985ed4777b7e66cffe46d6928300
maro <maronn.jan@gmail.com>
2021-11-04 14:45:57 +0900

    test用のシーン作成

--- Assets/tanaka/script/Button_seni_Tanaka.cs
+++ Assets/tanaka/script/Button_seni_Tanaka.cs
@@ -18,13 +18,22 @@ public class Button_Seni_Tanaka : MonoBehaviour
     {
 
     }
-    public void OnClick_Genchi()
+    public void OnClick_Genchi()//現地画面飛ぶ（テスト運用）
     {
         SceneManager.LoadScene("Genchi");
     }
 
-    public void OnClick_Homepage()
+    public void OnClick_Homepage()//ホームページに遷移
     {
         SceneManager.LoadScene("homepage");
+    }
+
+    public void OnClick_Flymo()//fly.m.oから出る（今いる現地に戻る？）テスト運用
+    {
+        SceneManager.LoadScene("test");
+    }
+    public void OnClick_Sekai()
+    {
+        SceneManager.LoadScene("SekaiGamen");
     }
 }
