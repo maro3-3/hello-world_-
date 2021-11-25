ecf480d4f411d93050b01c3a9909b0cf7411d900
maro <maronn.jan@gmail.com>
2021-11-25 22:37:55 +0900

    修正

--- Assets/tanaka/script/Debug_Tanaka.cs
+++ Assets/tanaka/script/Debug_Tanaka.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Debug_Tanaka : MonoBehaviour
 {
-    [SerializeField] Area_Tanaka database = null;
+    [SerializeField] DataBase_Tanaka database = null;
     // Start is called before the first frame update
     void Start()
     {
-        int unemployedNum = database.area;
-        string unemployedNumS = unemployedNum.ToString();
-        gameObject.GetComponent<Text>().text = unemployedNumS;
+        //int unemployedNum = database.area;
+        //string unemployedNumS = unemployedNum.ToString();
+        //gameObject.GetComponent<Text>().text = unemployedNumS;
     }
 
     // Update is called once per frame
