e03ffcf58b11c9fa7a21c720aa85044d8f816d58
LAPTOP-E79MLAEH\kh534 <kh.534061@gmail.com>
2021-12-02 12:21:09 +0900

    ホームページ進捗：カネコ

--- Assets/Kaneko/Script/Datatext_kaneko.cs
+++ Assets/Kaneko/Script/Datatext_kaneko.cs
@@ -11,8 +11,26 @@ public class Datatext_kaneko : MonoBehaviour
     [SerializeField] private Text EmploymentNumT;
     [SerializeField] private Text PossessionDroneNumT;
     [SerializeField] private Text TempingDroneNumT;
-    [SerializeField] private Text TurnNumT;
+    [SerializeField] private Text TurnNumT;// ƒ^[ƒ“
 
+    // ¸‹ÆÒ
+    [SerializeField] private Text   CHN_Beijing_AreaLv;                            // ’†‘–k‹’nˆæƒŒƒxƒ‹
+    [SerializeField] private Text   CHN_Beijing_EmploymentNum;                     // ’†‘–k‹ŒÙ—p”
+    [SerializeField] private Text[] CHN_Beijing_TotalUnemployedNum = new Text[2];  // ’†‘–k‹“o˜^Ò”
+    [SerializeField] private Text   CHN_Beijing_Death_tollNum;                     // ’†‘–k‹€–SÒ”
+    [SerializeField] private Text[] CHN_Beijing_name = new Text[2];                // ’†‘–k‹–¼‘O
+    [SerializeField] private Text[] CHN_Beijing_LaborForce = new Text[2];          // ’†‘–k‹˜J“­—Í
+    [SerializeField] private Text[] CHN_Beijing_Totalassets = new Text[2];         // ’†‘–k‹‘‘Y
+
+    // ¶YÒ
+    [SerializeField] private Text[] CHN_Beijing_manuNameList  = new Text[2];   // ’†‘–k‹¶YÒ–¼‘O
+    [SerializeField] private Text[] CHN_Shanghai_manuNameList = new Text[2];   // ’†‘ãŠC¶YÒ–¼‘O
+    [SerializeField] private Text[] CHN_Sichuan_manuNameList  = new Text[5];   // ’†‘lì¶YÒ–¼‘O
+
+    [SerializeField] private Text[] CHN_Beijing_manufacturerName  = new Text[2];        // ’†‘–k‹¶YÒ–¼‘O
+    [SerializeField] private Text[] CHN_Beijing_manuLaborForce    = new Text[2];          // ’†‘–k‹˜J“­—Í
+    [SerializeField] private Text[] CHN_Beijing_manuAmountofSales = new Text[2];       // ’†‘–k‹˜J“­—Í
+    [SerializeField] private Text[] CHN_Beijing_manuTotalassets   = new Text[2];         // ’†‘–k‹‘‘Y
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +41,33 @@ public class Datatext_kaneko : MonoBehaviour
         PossessionDroneNumT.text = database.PossessionDroneNum.ToString();
         TempingDroneNumT.text = database.TempingDroneNum.ToString();
         TurnNumT.text = database.TurnNum.ToString();
+
+        CHN_Beijing_AreaLv.text = database.unemployed[0].UnemployedAreaNo.ToString();
+        CHN_Beijing_EmploymentNum.text = database.unemployed[0].EmploymentNum.ToString();
+        CHN_Beijing_TotalUnemployedNum[0].text = database.unemployed[0].InitUnemployedNum.ToString();
+        CHN_Beijing_TotalUnemployedNum[1].text = database.unemployed[0].CurrentUnemployedNum.ToString();
+        CHN_Beijing_Death_tollNum.text = database.unemployed[0].Death_tollNum.ToString();
+        CHN_Beijing_name[0].text = database.manufacturers[0].ManufacturerName.ToString();
+        CHN_Beijing_LaborForce[0].text = database.manufacturers[0].LaborForce.ToString();
+        CHN_Beijing_Totalassets[0].text = database.manufacturers[0].Totalassets.ToString();
+        CHN_Beijing_name[1].text = database.manufacturers[1].ManufacturerName.ToString();
+        CHN_Beijing_LaborForce[1].text = database.manufacturers[1].LaborForce.ToString();
+        CHN_Beijing_Totalassets[1].text = database.manufacturers[1].Totalassets.ToString();
+
+        CHN_Beijing_manuNameList[0].text = database.manufacturers[0].ManufacturerName.ToString();
+        CHN_Beijing_manuNameList[1].text = database.manufacturers[1].ManufacturerName.ToString();
+        CHN_Shanghai_manuNameList[0].text = database.manufacturers[2].ManufacturerName.ToString();
+        CHN_Shanghai_manuNameList[1].text = database.manufacturers[3].ManufacturerName.ToString();
+        CHN_Sichuan_manuNameList[0].text = database.manufacturers[4].ManufacturerName.ToString();
+        CHN_Sichuan_manuNameList[1].text = database.manufacturers[5].ManufacturerName.ToString();
+        CHN_Sichuan_manuNameList[2].text = database.manufacturers[6].ManufacturerName.ToString();
+        CHN_Sichuan_manuNameList[3].text = database.manufacturers[7].ManufacturerName.ToString();
+        CHN_Sichuan_manuNameList[4].text = database.manufacturers[8].ManufacturerName.ToString();
+
+        CHN_Beijing_manufacturerName[0].text = database.manufacturers[0].ManufacturerName.ToString();
+        CHN_Beijing_manuLaborForce[0].text = database.manufacturers[0].LaborForce.ToString();
+        CHN_Beijing_manuAmountofSales[0].text = database.manufacturers[0].AmountofSales.ToString();
+        CHN_Beijing_manuTotalassets[0].text = database.manufacturers[0].Totalassets.ToString();
     }
 
     // Update is called once per frame
