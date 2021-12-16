ef94ec790a6538e664b7014ee2e4b2e48545bad8
髙橋　智裕 <takaaki.anuzik@gmail.com>
2021-12-16 12:21:30 +0900

    画像データ

--- Assets/Anuzik/testtest/button_testtest.cs
+++ Assets/Anuzik/testtest/button_testtest.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class button_testtest : MonoBehaviour
 {
+
+    public Button[] button_ishikado = new Button[1];
+
+    Collider2D col;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +21,19 @@ public class button_testtest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        OnTriggerStay2D(col);
+
+
+
+
     }
 
 
-
+    void OnTriggerStay2D(Collider2D col)
+    {
+        Debug.Log("“–‚½‚Á‚½");
+    }
 
 
 
