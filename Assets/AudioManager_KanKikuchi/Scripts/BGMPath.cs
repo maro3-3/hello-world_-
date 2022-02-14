178f4c2c46188dbf824a5e61aaaa50ef53ece487
maro <maronn.jan@gmail.com>
2022-02-14 17:48:54 +0900

    BGM周り修正

--- Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,15 +5,14 @@
 /// </summary>
 public static class BGMPath{
 
-	public const string BATTLE                = "BGM/Battle";
-	public const string BATTLE27              = "BGM/Battle/battle27";
-	public const string FANTASY14             = "BGM/fantasy14";
-	public const string HEARTBEAT01           = "BGM/heartbeat01";
-	public const string RESULT_LOSE           = "BGM/ResultLose";
-	public const string RESULT_WIN            = "BGM/ResultWin";
-	public const string SCENARIO              = "BGM/scenario";
-	public const string STANDBY               = "BGM/Standby";
-	public const string TITLE                 = "BGM/Title";
+	public const string BATTLE        = "BGM/Battle";
+	public const string GENCHI_HINKON = "BGM/GenchiHinkon";
+	public const string RESULT_LOSE   = "BGM/ResultLose";
+	public const string RESULT_WIN    = "BGM/ResultWin";
+	public const string SCENARIO      = "BGM/Scenario";
+	public const string SEKAI_GAMEN   = "BGM/SekaiGamen";
+	public const string STANDBY       = "BGM/Standby";
+	public const string TITLE         = "BGM/Title";
 
 }
 
